c1ce12813cbb5fe90f4269b4e058e285750e0dd2
Valentina Vidal <Valentinavisa@unisabana.edu.co>
2026-04-06 17:05:34 -0500

    ajaja

---
+++ overall.py
@@ -0,0 +1,25 @@
+def student_averages(students):
+    result = {}
+
+    for student, assignments in students.items():
+        total = sum(assignments.values())
+        count = len(assignments)
+        average = round(total / count)
+        result[student] = average
+
+    return result
+
+
+def assignment_averages(students):
+    result = {}
+    for assignment in next(iter(students.values())).keys():
+        total = 0
+        count = 0
+
+        for student in students:
+            total += students[student][assignment]
+            count += 1
+
+        result[assignment] = round(total / count)
+
+    return result
